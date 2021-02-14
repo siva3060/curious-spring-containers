77710a1456e0cbe35cb02b02fdd5a4029868d91f
siva3060 <siva3060@gmail.com>
2021-02-14 15:32:48 +0530

    Intial commit

---
+++ src/main/java/com/example/piggy/PiggyApplication.java
@@ -0,0 +1,13 @@
+package com.example.piggy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiggyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiggyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/piggy/PiggyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.piggy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiggyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
